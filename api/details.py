3db176cd0d91042fb58b1fbd027e969180947292
Paul Wells <wellsy95@hotmail.com>
2024-04-12 17:09:06 +0100

    blueprint done, working on null values in put program_exercise bug

---
+++ api/details.py
@@ -0,0 +1,90 @@
+from flask import Blueprint, session, render_template, redirect, request, jsonify, url_for
+import os
+from db import *
+from datetime import datetime
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+template_dir = os.path.join(current_dir, '..', 'templates')
+
+details_bp = Blueprint('details', __name__, url_prefix='/details', template_folder=template_dir)
+
+
+# adding exercises to a program
+@details_bp.route('/', methods=["POST"])
+def details():
+
+    logged_in_user = session.get('logged_in_user')
+    if not logged_in_user:
+        return render_template("not_authorised.html")
+
+    if request.method == "POST":
+        
+        try: 
+            form_data = request.form.to_dict()
+            exercise_id = form_data.get('exercise_id')
+        
+            user_id = logged_in_user['id']
+            program_id = form_data.get('program_id')
+
+            notes = form_data.get('notes')
+            sets = int(form_data.get('sets')) if form_data.get('sets') != '' else None
+            reps = int(form_data.get('reps')) if form_data.get('reps') != '' else None
+            
+            date_modified = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+            rating = int(form_data.get('rating')) if form_data.get('rating') != '' else None
+            result, status_code = db_block(add_exercise_to_program, program_id, exercise_id, notes, sets, reps, rating, date_modified, logged_in_user)
+
+            # DEV ONLY
+            user_agent = request.headers.get('User-Agent', '')
+            if 'Postman' in user_agent:
+                return jsonify({'result': result})
+
+            return redirect(f'/programs/program/{user_id}/{program_id}')
+
+        except:
+            return redirect(f'/programs/program/{user_id}/{program_id}')
+
+
+# removing / updating exercises in a program
+@details_bp.route('/<int:exercise_id>/<int:program_id>', methods=["PUT", "DELETE"])
+def update_or_remove_program_exercise(exercise_id, program_id):
+    logged_in_user = session.get('logged_in_user')
+    if not logged_in_user:
+        return render_template("not_authorised.html")
+    else:
+        user_id = logged_in_user['id']
+    
+    if request.method == "PUT":
+        
+        # try:
+            fields = ("notes", "sets", "reps", "rating")
+            data = process_request(request, *fields)
+
+            data['lastmod'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+            for key in ["sets", "reps", "rating"]: 
+                if data[key] == '' or not data[key].isnumeric():
+                    data[key] = None
+
+            query = "SET "
+            num_fields = len(data)
+            for index, (key, value) in enumerate(data.items()):
+                query += f"{key} = '{value}'"
+                if index < num_fields - 1:
+                    query += ", "
+            
+
+            result, status_code = db_block(update_exercise_in_program, program_id, exercise_id, logged_in_user, query)
+
+            return result, status_code
+        #except:
+            #return redirect(f'/programs/program/{user_id}/{program_id}')
+            
+
+    if request.method == "DELETE":
+        try:
+            result, status_code = db_block(delete_exercise_from_program, program_id, exercise_id, logged_in_user)
+
+            return result, status_code
+        except:
+            return redirect(f'/programs/program/{user_id}/{program_id}')
